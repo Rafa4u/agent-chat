5a9bd1943c261648e662cde1d7f67fa041350584
rafa s <103384324+Rafa4u@users.noreply.github.com>
2025-07-31 08:39:44 -0300

    nome assistente Update chat-didi.js

--- chat-didi.js
+++ chat-didi.js
@@ -127,7 +127,7 @@
   container.innerHTML = `
     <div id="chat-button"></div>
     <div id="chat-box">
-      <div id="chat-header"> 👩🏻 Didi - Agent-AI</div>
+      <div id="chat-header"> 👩🏻 Didi Assistente com AI</div>
       <div id="chat-messages"></div>
       <div id="chat-input">
         <input type="text" id="user-input" placeholder="Digite sua mensagem...">
